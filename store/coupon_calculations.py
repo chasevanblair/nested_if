3bf377608fe2c4412b6b21e784a2fe10c2f19b1c
chasevanblair <crvanblair@dmacc.edu>
2020-06-09 21:21:40 -0500

    between 10 30

--- store/coupon_calculations.py
+++ store/coupon_calculations.py
@@ -1,5 +1,16 @@
 def calculate_price(price, cash_coupon, percent_coupon):
-    pass
+    shipping = 0
+    if price < 10:
+        shipping = 5.95
+    elif price < 30:
+        shipping = 7.95
+    elif price < 50:
+        shipping = 11.95
 
+    if price - cash_coupon <= 0:
+        final_price = shipping * 1.06
+    else:
+        final_price = ((price - cash_coupon) * (1 - percent_coupon / 100) + shipping) * 1.06
+    return final_price
 
 #if __name__ == '__main__':
--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -10,3 +10,12 @@ def test_price_under_ten():
     assert calculate_price(3, 5, 15)
     assert calculate_price(5, 10, 15)
 
+
+def test_price_under_between_ten_thirty():
+    assert calculate_price(12, 5, 10)
+    assert calculate_price(22, 5, 15)
+    assert calculate_price(15, 5, 20)
+    assert calculate_price(26, 10, 10)
+    assert calculate_price(25, 10, 15)
+    assert calculate_price(14, 5, 15)
+    assert calculate_price(10, 10, 15)
